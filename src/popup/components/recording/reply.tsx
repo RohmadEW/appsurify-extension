b3b33824fce75680ba677c1c0d7592bb93ad792d
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-10-09 09:11:27 +0700

    reply

--- src/popup/components/recording/reply.tsx
+++ src/popup/components/recording/reply.tsx
@@ -3,7 +3,7 @@ import rrwebPlayer from "rrweb-player"
 
 import "rrweb-player/dist/style.css"
 
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
 
 import { useStorage } from "@plasmohq/storage/hook"
 
@@ -13,27 +13,34 @@ import { StorageKey } from "~types/storage"
 import icon from "/assets/icon.png"
 
 export default function ReplyRecording() {
-  const ref = useRef(null)
   const [, setRouterPage] = useStorage<ROUTE_PAGE>(StorageKey.ROUTE_PAGE)
   const [rrwebData] = useStorage<eventWithTime[]>(
     StorageKey.RRWEB_DATA,
     (value) => value ?? []
   )
 
-  useEffect(() => {
+  const reply = () => {
     if (rrwebData.length > 0) {
-      ref.current = new rrwebPlayer({
+      new rrwebPlayer({
         target: document.getElementById("replay-container"),
         props: {
-          events: [],
+          events: rrwebData,
+          autoPlay: true,
           skipInactive: true,
-          width: 100,
-          height: 100,
-          autoPlay: true
+          width: 400,
+          height: 300
         }
       })
     }
+  }
+
+  useEffect(() => {
+    reply()
   }, [])
+
+  useEffect(() => {
+    reply()
+  }, [rrwebData])
 
   return (
     <div className="plasmo-pt-6 plasmo-pb-4 plasmo-px-12">
@@ -44,7 +51,12 @@ export default function ReplyRecording() {
       <div className="plasmo-text-3xl plasmo-mt-4 plasmo-text-center">
         Reply Recording
       </div>
-      <div id="replay-container" style={{ width: "100%", height: "300px" }} />
+      <div className="plasmo-text-center">
+        There are {rrwebData.length} events recorded
+      </div>
+      <div className="plasmo-mt-4 plasmo-border plasmo-border-gray-100 plasmo-shadow-md plasmo-overflow-x-auto">
+        <div id="replay-container" style={{ width: "100%", height: "300px" }} />
+      </div>
       <button
         className="plasmo-btn plasmo-btn-outline plasmo-w-full plasmo-mt-8"
         onClick={() => setRouterPage(ROUTE_PAGE.RECORDING)}>
