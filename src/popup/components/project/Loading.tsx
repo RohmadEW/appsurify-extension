4f69c59cc6aad8f8c7fe7a449c76021c65951504
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-11-15 06:45:42 +0700

    fix

--- src/popup/components/project/Loading.tsx
+++ src/popup/components/project/Loading.tsx
@@ -4,7 +4,7 @@ export const LoadingProject = () => {
       <div className="plasmo-mt-[32px] plasmo-text-[16px] plasmo-text-[#757575]">
         Projects
       </div>
-      <div>
+      <div className="plasmo-mt-[16px]">
         <div className="plasmo-skeleton plasmo-w-full plasmo-h-[50px] plasmo-mt-[16px] plasmo-rounded-md"></div>
         <div className="plasmo-skeleton plasmo-w-full plasmo-h-[50px] plasmo-mt-[16px] plasmo-rounded-md"></div>
         <div className="plasmo-skeleton plasmo-w-full plasmo-h-[50px] plasmo-mt-[16px] plasmo-rounded-md"></div>
